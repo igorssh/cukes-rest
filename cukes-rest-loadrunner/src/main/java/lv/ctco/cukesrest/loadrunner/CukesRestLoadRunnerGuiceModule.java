78440b9a01289d2fe006d36adcc45d3fee142538
Igor Gursky <Igor_Gursky@rcomext.com>
2017-06-14 15:02:53 +0300

    LoadRunner

--- cukes-rest-loadrunner/src/main/java/lv/ctco/cukesrest/loadrunner/CukesRestLoadRunnerGuiceModule.java
+++ cukes-rest-loadrunner/src/main/java/lv/ctco/cukesrest/loadrunner/CukesRestLoadRunnerGuiceModule.java
@@ -5,12 +5,18 @@ import lv.ctco.cukescore.extension.CukesInjectableModule;
 import lv.ctco.cukescore.internal.AssertionFacade;
 import lv.ctco.cukesrest.VariableFacade;
 
+import static lv.ctco.cukescore.CukesOptions.LOADRUNNER_FILTER_BLOCKS_REQUESTS;
+import static lv.ctco.cukescore.CukesOptions.PROPERTIES_PREFIX;
+
 @CukesInjectableModule
 public class CukesRestLoadRunnerGuiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(AssertionFacade.class).to(AssertionFacadeLoadRunnerImpl.class);
-        bind(VariableFacade.class).to(VariableFacadeLoadRunnerImpl.class);
+        boolean isLoadRunnedEnabled = Boolean.valueOf(System.getProperty(PROPERTIES_PREFIX + LOADRUNNER_FILTER_BLOCKS_REQUESTS));
+        if (isLoadRunnedEnabled) {
+            bind(AssertionFacade.class).to(AssertionFacadeLoadRunnerImpl.class);
+            bind(VariableFacade.class).to(VariableFacadeLoadRunnerImpl.class);
+        }
     }
 }
--- cukes-rest/src/main/java/lv/ctco/cukesrest/CukesRestGuiceModule.java
+++ cukes-rest/src/main/java/lv/ctco/cukesrest/CukesRestGuiceModule.java
@@ -5,12 +5,19 @@ import lv.ctco.cukescore.extension.CukesInjectableModule;
 import lv.ctco.cukescore.internal.AssertionFacade;
 import lv.ctco.cukescore.internal.AssertionFacadeImpl;
 
+import static lv.ctco.cukescore.CukesOptions.LOADRUNNER_FILTER_BLOCKS_REQUESTS;
+import static lv.ctco.cukescore.CukesOptions.PROPERTIES_PREFIX;
+
 @CukesInjectableModule
 public class CukesRestGuiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(AssertionFacade.class).to(AssertionFacadeImpl.class);
-        bind(VariableFacade.class).to(VariableFacadeImpl.class);
+
+        boolean isLoadRunnedEnabled = Boolean.valueOf(System.getProperty(PROPERTIES_PREFIX + LOADRUNNER_FILTER_BLOCKS_REQUESTS));
+        if (!isLoadRunnedEnabled) {
+            bind(AssertionFacade.class).to(AssertionFacadeImpl.class);
+            bind(VariableFacade.class).to(VariableFacadeImpl.class);
+        }
     }
 }
